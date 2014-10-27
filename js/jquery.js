2a01bd774469f96b82335d24a12d7e0f6597dd22
Stuart_Grossman <grossman.stuart1@gmail.com>
2014-10-27 15:50:04 -0700

    done with clever function

--- js/jquery.js
+++ js/jquery.js
@@ -21,7 +21,6 @@ $(document).ready(function(){
         $('button').on('click', function(){
             var selected_word = $('#letter_input').val();
               check_event(selected_word, password)
-              console.log(result);
               switch(result){
                 case 0:
                     $('#message').html("Correct Password");
@@ -46,38 +45,4 @@ $(document).ready(function(){
                     break
               };
         });
-
-        // $('#button').on('click', function(){
-        //     var selected_word = $('#letter_input').val();
-        //     if(selected_word == password){
-        //          // console.log(selected_word.length)
-        //          $('#message').html("Correct Password");
-        //          $('#message').css({'color' : 'green'});
-        //          $('#overlay').css({'opacity' : '0.7', 'background' : '#FFF'});
-        //          $('#boxShape').hide();
-        //          $('#resume').fadeIn(1600);
-        //     };
-        // });
-        // $('#button').on('click', function(){
-        //     var selected_word = $('#letter_input').val();
-        //     if(selected_word.length === 0){
-        //         // console.log(selected_word.length)
-        //         $('#resume').fadeOut(1600);
-        //         $('#message').html("Please enter a Password");
-        //         $('#message').css({'color' : 'black'});
-        //         $('#overlay').css({'opacity' : '1.0', 'background' : '#000'});
-        //         $('#boxShape').show();
-        //     };
-        // });
-        // $('#button').on('click', function(){
-        //     var selected_word = $('#letter_input').val();
-        //     if(selected_word != password && selected_word.length >= 1){
-        //         // console.log(selected_word.length)
-        //         $('#resume').fadeOut(1600);
-        //         $('#message').html("Incorrect Password");
-        //         $('#message').css({'color' : 'red'});
-        //         $('#overlay').css({'opacity' : '1.0', 'background' : '#000'});
-        //         $('#boxShape').show();
-        //     };
-        // });
 });
